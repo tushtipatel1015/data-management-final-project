3cc25d67d41585d8da1ff51085927dcfa3b237a5
Kashish Patel <patelkashish1703@gmail.com>
2026-05-02 11:56:01 -0400

    Updated logic

--- scripts/predict_flight.py
+++ scripts/predict_flight.py
@@ -2,27 +2,70 @@ import pandas as pd
 from model import train_model
 
 def get_user_input():
-    print("Enter flight information:")
+    print("Enter flight information:\n")
 
-    sample = {
-        "MONTH": int(input("Month (1-12): ")),
-        "DAY_OF_WEEK": int(input("Day of week (0=Mon, 6=Sun): ")),
-        "DEP_HOUR": int(input("Departure hour (0-23): ")),
-        "OP_CARRIER": input("Airline code (ex: UA, AA, DL): ").upper(),
-        "ORIGIN": input("Origin airport code (ex: SFO): ").upper(),
-        "DEST": input("Destination airport code (ex: LAX): ").upper(),
-        "DISTANCE": float(input("Distance in miles: ")),
-        "precipitation": float(input("Precipitation: ")),
-        "avg_temp": float(input("Average temperature: ")),
-        "avg_wind_speed": float(input("Average wind speed: ")),
-        "fog": int(input("Fog? (1=yes, 0=no): ")),
-        "heavy_fog": int(input("Heavy fog? (1=yes, 0=no): ")),
-        "thunder": int(input("Thunder? (1=yes, 0=no): ")),
-        "hail": int(input("Hail? (1=yes, 0=no): ")),
-        "smoke_or_haze": int(input("Smoke or haze? (1=yes, 0=no): "))
-    }
+    month = int(input("Month (1-12): "))
 
-    return pd.DataFrame([sample])
+    print("\nDay of week:")
+    print("0 = Monday, 1 = Tuesday, ..., 6 = Sunday")
+    day_of_week = int(input("Enter day of week: "))
+
+    dep_hour = int(input("\nDeparture hour (0-23): "))
+
+    carrier = input("\nAirline code (UA, AA, DL, etc.): ").upper()
+    origin = input("Origin airport (ex: SFO): ").upper()
+    dest = input("Destination airport (ex: LAX): ").upper()
+
+    print("\nWeather condition:")
+    print("1 = Clear")
+    print("2 = Rain")
+    print("3 = Fog")
+    print("4 = Storm")
+    print("5 = Snow")
+
+    weather_choice = int(input("Choose weather type: "))
+
+    # Default values
+    precipitation = 0
+    avg_temp = 60
+    avg_wind_speed = 5
+    fog = 0
+    heavy_fog = 0
+    thunder = 0
+    hail = 0
+    smoke_or_haze = 0
+
+    # Map weather to model features
+    if weather_choice == 2:  # Rain
+        precipitation = 0.5
+    elif weather_choice == 3:  # Fog
+        fog = 1
+    elif weather_choice == 4:  # Storm
+        precipitation = 1.0
+        thunder = 1
+        avg_wind_speed = 15
+    elif weather_choice == 5:   # Snow
+        precipitation = 0.8
+        avg_temp = 30
+        avg_wind_speed = 10
+
+    return pd.DataFrame([{
+        "MONTH": month,
+        "DAY_OF_WEEK": day_of_week,
+        "DEP_HOUR": dep_hour,
+        "OP_CARRIER": carrier,
+        "ORIGIN": origin,
+        "DEST": dest,
+        "DISTANCE": 500,  # default (since user won't know)
+        "precipitation": precipitation,
+        "avg_temp": avg_temp,
+        "avg_wind_speed": avg_wind_speed,
+        "fog": fog,
+        "heavy_fog": heavy_fog,
+        "thunder": thunder,
+        "hail": hail,
+        "smoke_or_haze": smoke_or_haze
+    }])
 
 
 def main():
