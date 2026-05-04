3bcd9899eeee27d0aa7b7b02bc6a6de363d2a2e8
Kashish Patel <patelkashish1703@gmail.com>
2026-05-04 12:27:14 -0400

    Changes

--- scripts/clean_flights.py
+++ scripts/clean_flights.py
@@ -3,42 +3,49 @@ import glob
 import os
 
 def clean_flights():
-    files = glob.glob("split_flights/*.csv")    # find all split flight csv files
     
+    # find all flight files (monthly split)
+    files = glob.glob("split_flights/*.csv")
+    
+    # if not file is found print a message
     if not files:
         print("No files found in split_flights/")
         return
     
-    dataframe_list = [] # load and combine all monthly files
+    # to combine each monthly file into one dataframe
+    flight_dataframe_list = []
     
     for file in files:
         dataframe = pd.read_csv(file)
-        dataframe_list.append(dataframe)
+        flight_dataframe_list.append(dataframe)
     
-    flights = pd.concat(dataframe_list, ignore_index = True)
+    flights = pd.concat(flight_dataframe_list, ignore_index = True)
     
-    # drop unnamed section data
+    # Step 1: drop extra columns "Unnamed" to prevent unnecessary analysis
     flights = flights.drop(columns = ["Unnamed: 27"], errors = "ignore")
     
-    # convert date column to datetime
+    # Step 2: convert flight date to a common datetime format
     flights["FL_DATE"] = pd.to_datetime(flights["FL_DATE"], errors = "coerce")
     
-    # safety check to ensure that we only consider 
+    # only consider flights coming to or going from SFO airport
     flights = flights[
         (flights["ORIGIN"] == "SFO") |
         (flights["DEST"] == "SFO")
     ]
     
-    flights = flights.dropna(subset = ["FL_DATE"])  # remove rows where date is missing
+    # Step 3: remove rows where the date is missing
+    flights = flights.dropna(subset = ["FL_DATE"]) 
     
-    flights["IS_DELAYED"] = (flights["ARR_DELAY"] > 15).astype(int) # create target columns
+    # do only if flight arrives more than 15 mins late
+    flights["IS_DELAYED"] = (flights["ARR_DELAY"] > 15).astype(int)
     
-    # create useful time features
+    # creation of time based features for easier reading of data for analysis and prediction
     flights["MONTH"] = flights["FL_DATE"].dt.month
     flights["DAY_OF_WEEK"] = flights["FL_DATE"].dt.dayofweek
     flights["DEP_HOUR"] = flights["CRS_DEP_TIME"] // 100
     
-    os.makedirs("data", exist_ok = True)
+    # save cleaned flight data in cleaned_flights.csv
+    os.makedirs("data/cleaned", exist_ok = True)    # safety check to ensure cleaned folder exists
     flights.to_csv("cleaned_flights.csv", index = False)
     
     print("Cleaned flights file saved to cleaned_flights.csv")
--- scripts/create_database.py
+++ scripts/create_database.py
@@ -1,25 +1,30 @@
 import pandas as pd
 import sqlite3
+import os
 
 def create_database():
+    
+    # load cleaned dataset csv files into this one, to help create database
+    flights_file = pd.read_csv("data/cleaned/cleaned_flights.csv")
+    weather_file = pd.read_csv("data/cleaned/Cleaned_SFO_2018_Climate.csv")
+    
+    # safety check to ensure database folder exists
+    os.makedirs("data/database", exist_ok = True)
 
-    # load cleaned datasets
-    flights = pd.read_csv("data/cleaned/cleaned_flights.csv")
-    weather = pd.read_csv("data/cleaned/Cleaned_SFO_2018_Climate.csv")
-
-    # create database connection
+    # creating SQLite database connection
     conn = sqlite3.connect("data/database/flights_weather.db")
 
-    # write tables into database
-    flights.to_sql("Flights", conn, if_exists="replace", index=False)
-    weather.to_sql("Weather", conn, if_exists="replace", index=False)
+    # storing as tables in the SQLite database
+    flights_file.to_sql("Flights", conn, if_exists="replace", index=False)
+    weather_file.to_sql("Weather", conn, if_exists="replace", index=False)
 
-    conn.close()
-
+    conn.close()    # close connection once done
+    
+    # print everything at the end as a confirmation
     print("Database created successfully.")
     print("Tables created:")
-    print(" - Flights")
-    print(" - Weather")
+    print(" 1. Flights")
+    print(" 2. Weather")
 
 if __name__ == "__main__":
     create_database()
