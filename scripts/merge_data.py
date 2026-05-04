32aff91dac80a10b99848502de207dcbe38f4dfc
Kashish Patel <patelkashish1703@gmail.com>
2026-05-04 12:56:12 -0400

    Changes

--- scripts/merge_data.py
+++ scripts/merge_data.py
@@ -7,12 +7,12 @@ def merge_data():
     flights_file = pd.read_csv("data/cleaned/cleaned_flights.csv")
     weather_file = pd.read_csv("data/cleaned/Cleaned_SFO_2018_Climate.csv")
 
-    flights_file["FL_DATE"] = pd.to_datetime(flights["FL_DATE"], errors="coerce")
-    weather_flight["date"] = pd.to_datetime(weather["date"], errors="coerce")
+    flights_file["FL_DATE"] = pd.to_datetime(flights_file["FL_DATE"], errors="coerce")
+    weather_file["date"] = pd.to_datetime(weather_file["date"], errors="coerce")
     
     # merge flight and weather data, on date
-    merged = flights.merge(
-        weather,
+    merged = flights_file.merge(
+        weather_file,
         left_on="FL_DATE",
         right_on="date",
         how="left"
